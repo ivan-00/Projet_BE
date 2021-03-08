1fc73f519ba8f7df1387b580d9b1a3b8a3f2bad1
ivan-00 <IvanSery0@gmail.com>
2021-03-08 19:14:31 +0100

    v1 nav

--- config/template/nav.php
+++ config/template/nav.php
@@ -5,20 +5,20 @@
     <a class="nav-logo" href="">
         CBD HACE
     </a>
+    <div class="nav-item-center">
     <form action="" class="formulaire">
-        <input class="champ" type="text" value="Rechercher un produit"/>
-        <input class="bouton" type="button" value=" Rechercher" />
+        <input class="champ" type="text" placeholder ="Rechercher un produit"/>
+        <button type="submit"><i class="fa fa-search"></i></button>
     </form>
-    <div class="nav-item">
-        <ul>
+        <ul class="nav-item">
             <li><a href="/Projet_back-end/Projet_BE/categorie/fleur.php">Fleur CBD</a></li>
             <li><a href="/Projet_back-end/Projet_BE/categorie/vaporette.php">Nos Vaporisateur / E-Cigarette</a></li>
             <li><a href="/Projet_back-end/Projet_BE/categorie/huile.php">Nos huiles</a></li>
             <li><a href="/Projet_back-end/Projet_BE/categorie/documentation.php">Documentation</a></li>
         </ul>
     </div>
-    <div class="nav-compte">
-        <a href="/Projet_back-end/Projet_BE/page/panier.php"><i class="fas fa-shopping-cart"></i>Panier</a>
-        <a href="/Projet_back-end/Projet_BE/page/login.php"> <i class="fas fa-user"></i> Compte client</a>
-    </div>
+        <div class="nav-compte">
+            <a href="/Projet_back-end/Projet_BE/page/panier.php"><i class="fas fa-shopping-cart"></i>Panier</a>
+            <a href="/Projet_back-end/Projet_BE/page/login.php"> <i class="fas fa-user"></i> Compte client</a>
+        </div>
 </nav>
