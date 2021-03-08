de217a2432cbbb1ad1975112fd71db1d9e3465d5
ivan-00 <IvanSery0@gmail.com>
2021-03-08 17:29:27 +0100

    Changement Url de STYLE.CSS

--- config/template/head.php
+++ config/template/head.php
@@ -14,15 +14,11 @@ require 'config/init.php';
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="asset/style/style.css">
+    <link rel="stylesheet" href="/Projet_back-end/Projet_BE/asset/style/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300&display=swap" rel="stylesheet">
     <!-- Font awsome -->
     <script src="https://kit.fontawesome.com/f25e2f6a6c.js" crossorigin="anonymous"></script>
-
-    <!-- Fave Icon  -->
-
+    <!-- Style -->
 </head>
-
-<nav></nav>
 <body>
