7ad284551308cab1072b6f05f32e1465d97c71e0
ivan-00 <IvanSery0@gmail.com>
2021-03-09 02:03:24 +0100

    version 1 footer

--- config/template/footer.php
+++ config/template/footer.php
@@ -1,6 +1,46 @@
-<footer>je suis le footer</footer>
-</div>
+<?php
+    include 'Propjet_BE/config/template/head.php';
+?> 
 
+<footer>
+<div class="footer">
+  <div class="footer_left">
+  <h1 class="nav-logo" >
+        CBD HACE
+  </h1>
+  <div class="social">
+    <p>SUIVRE-NOUS!!</p>
+    <ul>
+      <li><a target="_blank"  href="https://fr-fr.facebook.com/"><i class="fab fa-facebook"></i></a></li>
+      <li><a target="_blank"  href="https://www.youtube.com/?hl=fr&gl=FR"><i class="fab fa-youtube"></i></a></li>
+      <li><a target="_blank"  href="https://twitter.com/?lang=fr"><i class="fab fa-twitter"></i></a></li>
+      <li><a target="_blank"  href="https://www.instagram.com/?hl=fr"><i class="fab fa-instagram"></i></a></li>
+    </ul>
+
+  </div>
+  </div>
+  <div class="footer_center">
+    <h1>HACE</h1>
+    <p>OUVERT DU <br>
+    Lundi au Samedi 9h30 - 20h00 <br>
+    Dimanche 11h - 20h00</p>
+    <p>01.40.40.40.40</p>
+    <br>
+    <a href="https://www.google.com/mars/#lat=-47.931066&lon=-138.867187&zoom=1&map=visible" target="_blank">12 rue de la Constélation, 75000</a>
+  </div>
+  <div class="footer_right">
+    <h3>INFORMATION</h3>
+    <ul>
+      <li><a href="#">NOUS CONTACTER</a></li>
+      <li><a href="#">OUVRIR UNE FRANCHISE</a></li>
+      <li><a href="#">DEVENIR PARTENAIRE</a></li>
+    </ul>
+  </div>
+</div>
+<div class="footer_bottom">
+  <p>© Crédits Matière 1ère - Mentions légales - Préférences cookies - <a href="#">Plan du site</a> </p>
+</div>
+</footer>
 <script src="asset/script/script.js"></script>
 </body>
 
