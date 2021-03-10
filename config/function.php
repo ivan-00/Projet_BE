e5570b4008c1690e3b8544617280a61594c0d218
ivan-00 <IvanSery0@gmail.com>
2021-03-10 17:41:10 +0100

    arianne

--- config/function.php
+++ config/function.php
@@ -1,1 +1,26 @@
 <?php
+/* Fil d'ariane */
+function ariane($classe) {
+  $def = "index";
+  $dPath = $_SERVER['PHP_SELF'];
+  $dChunks = explode("/", $dPath);
+  
+  echo('<a class="dynNav" href="/">Accueil</a><span class="dynNav"> > </span>');
+  for($i=1; $i<count($dChunks); $i++ ){
+    echo('<a class="dynNav" href="/');
+    for($j=1; $j<=$i; $j++ ){
+      echo($dChunks[$j]);
+      if($j!=count($dChunks)-1){ echo("/");}
+    }
+    
+    if($i==count($dChunks)-1){
+      $prChunks = explode(".", $dChunks[$i]);
+      if ($prChunks[0] == $def) $prChunks[0] = "";
+      $prChunks[0] = $prChunks[0] . "</a>";
+    }
+    else $prChunks[0]=$dChunks[$i] . '</a><span class="dynNav"> > </span>';
+    echo('">');
+    echo(str_replace("_" , " " , $prChunks[0]));
+  } 
+}
+?>
