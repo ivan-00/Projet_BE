fd9898ec0bc48adec60bae777a645d54086d1073
ivan-00 <IvanSery0@gmail.com>
2021-03-10 21:41:36 +0100

    style

--- config/template/head.php
+++ config/template/head.php
@@ -11,12 +11,12 @@ include '../init.php';
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <!-- Style -->
     <link rel="stylesheet" href="/Projet_BE/asset/style/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300&display=swap" rel="stylesheet">
     <!-- Font awsome -->
     <script src="https://kit.fontawesome.com/f25e2f6a6c.js" crossorigin="anonymous"></script>
-    <!-- Style -->
 </head>
 <body>
 </body>
