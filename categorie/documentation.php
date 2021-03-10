cb251dda66ed001b9fb74a1970a871b309316bcb
ivan-00 <IvanSery0@gmail.com>
2021-03-10 17:40:14 +0100

    banner image

--- categorie/documentation.php
+++ categorie/documentation.php
@@ -5,7 +5,10 @@
 
 include '../config/template/nav.php';
 ?>
-
+<div class="banner banner_documentation">
+  <h1>Notre Documentation</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates vitae pariatur animi itaque officiis dolore fugit ipsum soluta eum? Molestiae rem minus, velit accusamus consequatur beatae sequi eligendi esse officia!</p>
+</div>
 echo('Documentation');
 <?php
 include '../config/template/footer.php';
