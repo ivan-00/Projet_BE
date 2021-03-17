98ce160aacb84f2136f3fcce227cb4f0d36759ad
ivan-00 <IvanSery0@gmail.com>
2021-03-17 22:42:40 +0100

    php fiche produit

--- page/fiche_produit.php
+++ page/fiche_produit.php
@@ -2,15 +2,13 @@
 session_start();
 $bdd = new PDO("mysql:host=localhost;dbname=projetBackend;charset=utf8","root","root");
 ?>
-<?php require 'config/init.php';
-?>
 
 <?php
 $id= $_GET['produit'];
 if (filter_var($id,FILTER_VALIDATE_INT) == false) {
   echo 'erreur';
 }
-$produit = $bdd->query("SELECT * FROM product WHERE id_produit = '$id'");
+$produit = $bdd->query("SELECT * FROM product WHERE id_produit = '$id' ");
 if ($reponse = $produit->fetch()) {?>
 <h1> <?php echo $reponse['nom_produit'];  ?></h1>
   <?php
