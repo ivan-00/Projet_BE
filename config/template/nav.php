c4971e70b9c52169eebabbb993bca06e0a6ffe83
ivan-00 <IvanSery0@gmail.com>
2021-03-26 18:24:10 +0100

    erreur link

--- config/template/nav.php
+++ config/template/nav.php
@@ -1,3 +1,4 @@
+<body>
 <nav>
     <a class="nav-logo" href="/index.php">
         CBD HACE
@@ -11,19 +12,18 @@
         // echo $resultats;
     ?>
         <ul class="nav-item">
-            <li><a href="/categorie/fleur.php">Fleur CBD</a></li>
-            <li><a href="/categorie/vaporette.php">Nos Vaporisateurs / E-Cigarette</a></li>
-            <li><a href="/categorie/huile.php">Nos huiles</a></li>
-            <li><a href="/categorie/documentation.php">Documentation</a></li>
+            <li><a href="/Projet_BE/categorie/fleur.php">Fleur CBD</a></li>
+            <li><a href="/Projet_BE/categorie/vaporette.php">Nos Vaporisateurs / E-Cigarette</a></li>
+            <li><a href="/Projet_BE/categorie/huile.php">Nos huiles</a></li>
+            <li><a href="/Projet_BE/categorie/documentation.php">Documentation</a></li>
         </ul>
     </div>
-        <div class="nav-compte">
+    <div class="nav-compte">
             <div class="panier">
-                <a href="/page/panier.php"><i class="fas fa-shopping-cart"></i>Panier</a>
+                <a href="/Projet_BE/page/panier.php"><i class="fas fa-shopping-cart"></i>Panier</a>
             </div>
-
             <div class="compte">
-                <a href='/page/login.php'> <i class="fas fa-user"></i> Compte client</a>
+                <a href="/Projet_BE/page/login.php"> <i class="fas fa-user"></i> Compte client</a>
             </div>
         </div>
 </nav>
