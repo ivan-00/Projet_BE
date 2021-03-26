1f53ee2ee2339d9fbaa46a71e7696ca7ad73445b
ivan-00 <IvanSery0@gmail.com>
2021-03-26 11:20:17 +0100

    boutton PLUS MOINS

--- asset/script/dist/script.dev.js
+++ asset/script/dist/script.dev.js
@@ -1,1 +1,39 @@
-'use strict';
+"use strict";
+
+console.log('hello word'); // Fiche produit bouton ajouter au paneir.
+
+var btnPlus = document.getElementById('plus');
+var btnMoins = document.getElementById('moins');
+var res = document.getElementById('result');
+var result = parseInt(res.value, 10);
+var prixAffiche = document.getElementById('text_prix');
+var prixDepart = document.getElementById('text_prix');
+var prixFinal = prixAffiche.value;
+res.addEventListener('blur', function () {
+  result = document.getElementById('result');
+  result = parseInt(result.value, 10);
+});
+btnPlus.addEventListener('click', function () {
+  if (result >= 0 && result < 99) {
+    result++;
+    document.getElementById('result').value = result;
+
+    var _prixFinal = result * prixAffiche.innerHTML;
+
+    prixAffiche.innerHTML = _prixFinal;
+    console.log('prixFinal');
+  }
+});
+btnMoins.addEventListener('click', function () {
+  if (result > 0 && result <= 99) {
+    result--;
+    document.getElementById('result').value = result;
+
+    if (prixAffiche / 2) {
+      prixAffiche = prixDepart / 2;
+      prixAffiche.innerHTML = prixFinal;
+    } // let prixFinal   =  prixAffiche.innerHTML / result
+    // prixAffiche.innerHTML = prixFinal;
+
+  }
+}); // Calculer le prix.
--- asset/script/script.js
+++ asset/script/script.js
@@ -1,1 +1,41 @@
 "use strict";
+console.log ('hello word');
+// Fiche produit bouton ajouter au paneir.
+let btnPlus         = document.getElementById('plus');
+let btnMoins        = document.getElementById('moins');
+let res             = document.getElementById('result');
+let result          = parseInt(res.value,10);
+let prixAffiche     = document.getElementById('text_prix');
+let prixDepart      = document.getElementById('text_prix');
+let prixFinal       = prixAffiche.value;
+
+res.addEventListener('blur',function(){
+    result = document.getElementById('result');
+    result = parseInt(result.value,10);
+});
+
+btnPlus.addEventListener('click', function() {
+    if(result >= 0 && result < 99){
+        result++;
+        document.getElementById('result').value = result;
+        let prixFinal   = result * prixAffiche.innerHTML;
+        prixAffiche.innerHTML = prixFinal;
+        console.log('prixFinal');
+        
+    }
+});
+btnMoins.addEventListener('click', function(){
+    if(result > 0 && result <= 99){
+        result --;
+        document.getElementById('result').value = result;
+        if (prixAffiche / 2){
+            prixAffiche   = prixDepart / 2;
+            prixAffiche.innerHTML = prixFinal;
+        }
+        // let prixFinal   =  prixAffiche.innerHTML / result
+        // prixAffiche.innerHTML = prixFinal;
+    }
+});
+// Calculer le prix. 
+
+
