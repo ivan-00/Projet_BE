6dd9bc8bc6aaa1c559ecbb4b82733e018ba1daf7
ivan-00 <IvanSery0@gmail.com>
2021-03-30 16:14:17 +0200

    chnagement BDE

--- page/recherche.php
+++ page/recherche.php
@@ -3,8 +3,7 @@ session_start();
 require '../config/init.php';
 
 $mot = $_POST['query'];
-
-$search = $bdd->query("SELECT * FROM product WHERE nom_produit LIKE '% Fleur %' ");
+$search = $bdd->query("SELECT * FROM product WHERE nom_produit LIKE '% $mot %' ");
 if($reponse = $search->fetch()){
     echo "2";
 }
